ee605593bf747fb3b6905e8c6a48cf2165afccd6
Alban Crequy <alban@kinvolk.io>
2016-11-25 18:00:13 +0100

    add circle CI integration

--- build-kernel-builder.sh
+++ build-kernel-builder.sh
@@ -2,11 +2,11 @@
 set -x
 set -e
 
-sudo docker build -t alban/ebpf-kernel-builder -f Dockerfile.kernel-fedora-builder .
+sudo docker build -t kinvolk/ebpf-kernel-builder -f Dockerfile.kernel-fedora-builder .
 
 mkdir -p ebpf/
 
-sudo docker run -v $PWD:/host/ alban/ebpf-kernel-builder /bin/sh -c 'cp -r /src/ebpf/* /host/ebpf/'
+sudo docker run -v $PWD:/host/ kinvolk/ebpf-kernel-builder /bin/sh -c 'cp -r /src/ebpf/* /host/ebpf/'
 sudo chown -R $UID:$UID ebpf
 
 
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 sudo docker run --rm -it \
 	--privileged --net=host --pid=host \
 	-v /sys/kernel/debug:/sys/kernel/debug \
-	-v $PWD:/mnt \
-	alban/ebpf-user-env /bin/sh -c 'cd /mnt && ./trace_output_user ebpf/x86_64/4.8.8-200.fc24.x86_64/ebpf.o'
+	-ti \
+	kinvolk/ebpf-kprobe-example
