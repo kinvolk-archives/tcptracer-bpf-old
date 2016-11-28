5396028abbc45cc4403811a7985ee3afc21ef577
Alessandro Puccetti <alessandro@kinvolk.io>
2016-11-28 18:19:15 +0100

    fix formatting spaces vs tabs

--- trace_output_kern.c
+++ trace_output_kern.c
@@ -21,14 +21,14 @@ struct tcp_event_v4_t {
 };
 
 struct tcp_event_v6_t {
-    char ev_type[12];
-    u32 pid;
-    char comm[TASK_COMM_LEN];
-    unsigned __int128 saddr;
-    unsigned __int128 daddr;
-    u16 sport;
-    u16 dport;
-    u32 netns;
+	char ev_type[12];
+	u32 pid;
+	char comm[TASK_COMM_LEN];
+	unsigned __int128 saddr;
+	unsigned __int128 daddr;
+	u16 sport;
+	u16 dport;
+	u32 netns;
 };
 
 struct bpf_map_def SEC("maps") tcp_event_v4 = {
@@ -69,20 +69,20 @@ struct bpf_map_def SEC("maps") closesock = {
 SEC("kprobe/tcp_v4_connect")
 int kprobe__tcp_v4_connect(struct pt_regs *ctx)
 {
-    struct sock *sk;
-    u64 pid = bpf_get_current_pid_tgid();
-    u16 dport = 0;
-    char called_msg[] = "kprobe/tcp_v4_connect called\n";
+	struct sock *sk;
+	u64 pid = bpf_get_current_pid_tgid();
+	u16 dport = 0;
+	char called_msg[] = "kprobe/tcp_v4_connect called\n";
 
-    bpf_trace_printk(called_msg, sizeof(called_msg));
+	bpf_trace_printk(called_msg, sizeof(called_msg));
 
-    sk = (struct sock *) PT_REGS_PARM1(ctx);
+	sk = (struct sock *) PT_REGS_PARM1(ctx);
 
-    bpf_probe_read(&dport, sizeof(dport), &sk->__sk_common.skc_dport);
+	bpf_probe_read(&dport, sizeof(dport), &sk->__sk_common.skc_dport);
 
-    bpf_map_update_elem(&connectsock_v4, &pid, &sk, BPF_ANY);
+	bpf_map_update_elem(&connectsock_v4, &pid, &sk, BPF_ANY);
 
-    return 0;
+	return 0;
 }
 
 SEC("kretprobe/tcp_v4_connect")
@@ -148,18 +148,18 @@ int kretprobe__tcp_v4_connect(struct pt_regs *ctx)
 SEC("kprobe/tcp_v6_connect")
 int kprobe__tcp_v6_connect(struct pt_regs *ctx)
 {
-    struct sock *sk;
-    u64 pid = bpf_get_current_pid_tgid();
-    u16 dport = 0;
-    char called_msg[] = "kprobe/tcp_v6_connect called\n";
+	struct sock *sk;
+	u64 pid = bpf_get_current_pid_tgid();
+	u16 dport = 0;
+	char called_msg[] = "kprobe/tcp_v6_connect called\n";
 
-    bpf_trace_printk(called_msg, sizeof(called_msg));
+	bpf_trace_printk(called_msg, sizeof(called_msg));
 
-    sk = (struct sock *) PT_REGS_PARM1(ctx);
+	sk = (struct sock *) PT_REGS_PARM1(ctx);
 
-    bpf_probe_read(&dport, sizeof(dport), &sk->__sk_common.skc_dport);
+	bpf_probe_read(&dport, sizeof(dport), &sk->__sk_common.skc_dport);
 
-    bpf_map_update_elem(&connectsock_v6, &pid, &sk, BPF_ANY);
+	bpf_map_update_elem(&connectsock_v6, &pid, &sk, BPF_ANY);
 
     return 0;
 }
@@ -167,76 +167,76 @@ int kprobe__tcp_v6_connect(struct pt_regs *ctx)
 SEC("kretprobe/tcp_v6_connect")
 int kretprobe__tcp_v6_connect(struct pt_regs *ctx)
 {
-    int ret = PT_REGS_RC(ctx);
-    u64 pid = bpf_get_current_pid_tgid();
-    struct sock **skpp;
-    char called_msg[] = "kretprobe/tcp_v6_connect called\n";
+	int ret = PT_REGS_RC(ctx);
+	u64 pid = bpf_get_current_pid_tgid();
+	struct sock **skpp;
+	char called_msg[] = "kretprobe/tcp_v6_connect called\n";
 
-    bpf_trace_printk(called_msg, sizeof(called_msg));
+	bpf_trace_printk(called_msg, sizeof(called_msg));
 
-    skpp = bpf_map_lookup_elem(&connectsock_v6, &pid);
-    if (skpp == 0) {
-	return 0;	// missed entry
-    }
+	skpp = bpf_map_lookup_elem(&connectsock_v6, &pid);
+	if (skpp == 0) {
+		return 0;	// missed entry
+	}
 
-    if (ret != 0) {
-	// failed to send SYNC packet, may not have populated
-	// socket __sk_common.{skc_rcv_saddr, ...}
+	if (ret != 0) {
+		// failed to send SYNC packet, may not have populated
+		// socket __sk_common.{skc_rcv_saddr, ...}
+		bpf_map_delete_elem(&connectsock_v6, &pid);
+		return 0;
+	}
+
+	// pull in details
+	struct sock *skp = *skpp;
+	struct ns_common *ns;
+	unsigned __int128 saddr = 0, daddr = 0;
+	u32 net_ns_inum = 0;
+	u16 sport = 0, dport = 0;
+	bpf_probe_read(&sport, sizeof(sport), &((struct inet_sock *)skp)->inet_sport);
+	bpf_probe_read(&dport, sizeof(dport), &skp->__sk_common.skc_dport);
+
+	// if ports are 0, ignore
+	if (sport == 0 || dport == 0) {
+		bpf_map_delete_elem(&connectsock_v6, &pid);
+		return 0;
+	}
+
+	bpf_probe_read(&saddr, sizeof(saddr), &skp->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+	bpf_probe_read(&daddr, sizeof(daddr), &skp->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
+
+	// if addresses are 0, ignore
+	if (saddr == 0 || daddr == 0) {
+		bpf_map_delete_elem(&connectsock_v6, &pid);
+		return 0;
+	}
+
+	// Get network namespace id
+	possible_net_t skc_net;
+	bpf_probe_read(&skc_net, sizeof(skc_net), &skp->__sk_common.skc_net);
+	bpf_probe_read(&net_ns_inum, sizeof(net_ns_inum), &skc_net.net->ns.inum);
+
+	// output
+	struct tcp_event_v6_t evt = {
+		.ev_type = "connect",
+		.pid = pid >> 32,
+		.saddr = saddr,
+		.daddr = daddr,
+		// TODO alepuccetti: fix network byte order
+		.sport = sport,
+		.dport = dport,
+		.netns = net_ns_inum,
+	};
+
+	bpf_get_current_comm(&evt.comm, sizeof(evt.comm));
+
+	// do not send event if IP address is :: or port is 0
+	if (evt.saddr != 0 && evt.daddr != 0 && evt.sport != 0 && evt.dport != 0) {
+		bpf_perf_event_output(ctx, &tcp_event_v6, 0, &evt, sizeof(evt));
+	}
+
 	bpf_map_delete_elem(&connectsock_v6, &pid);
+
 	return 0;
-    }
-
-    // pull in details
-    struct sock *skp = *skpp;
-    struct ns_common *ns;
-    unsigned __int128 saddr = 0, daddr = 0;
-    u32 net_ns_inum = 0;
-    u16 sport = 0, dport = 0;
-    bpf_probe_read(&sport, sizeof(sport), &((struct inet_sock *)skp)->inet_sport);
-    bpf_probe_read(&dport, sizeof(dport), &skp->__sk_common.skc_dport);
-
-    // if ports are 0, ignore
-    if (sport == 0 || dport == 0) {
-	bpf_map_delete_elem(&connectsock_v6, &pid);
-	return 0;
-    }
-
-    bpf_probe_read(&saddr, sizeof(saddr), &skp->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
-    bpf_probe_read(&daddr, sizeof(daddr), &skp->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
-
-    // if addresses are 0, ignore
-    if (saddr == 0 || daddr == 0) {
-	bpf_map_delete_elem(&connectsock_v6, &pid);
-	return 0;
-    }
-
-    // Get network namespace id
-    possible_net_t skc_net;
-    bpf_probe_read(&skc_net, sizeof(skc_net), &skp->__sk_common.skc_net);
-    bpf_probe_read(&net_ns_inum, sizeof(net_ns_inum), &skc_net.net->ns.inum);
-
-    // output
-    struct tcp_event_v6_t evt = {
-	    .ev_type = "connect",
-	    .pid = pid >> 32,
-	    .saddr = saddr,
-	    .daddr = daddr,
-	    // TODO alepuccetti: fix network byte order
-	    .sport = sport,
-	    .dport = dport,
-	    .netns = net_ns_inum,
-    };
-
-    bpf_get_current_comm(&evt.comm, sizeof(evt.comm));
-
-    // do not send event if IP address is :: or port is 0
-    if (evt.saddr != 0 && evt.daddr != 0 && evt.sport != 0 && evt.dport != 0) {
-	bpf_perf_event_output(ctx, &tcp_event_v6, 0, &evt, sizeof(evt));
-    }
-
-    bpf_map_delete_elem(&connectsock_v6, &pid);
-
-    return 0;
 }
 
 SEC("kprobe/tcp_close")
