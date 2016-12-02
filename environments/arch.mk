e6b0570d5fc408264a5445aa8e9b9ff95fc8d059
Iago López Galeiras <iago@kinvolk.io>
2016-12-02 15:29:56 +0100

    fix arch build

--- environments/arch.mk
+++ environments/arch.mk
@@ -3,6 +3,6 @@ include build.mk
 DISTRO=arch
 
 KERNEL_VERSION=4.8.11-1-ARCH
-KERNEL_HEADERS=/usr/lib/modules/$(KERNEL_VERSION)
+KERNEL_HEADERS=/usr/lib/modules/$(KERNEL_VERSION)/build
 
 build: build-docker-image build-ebpf-object
