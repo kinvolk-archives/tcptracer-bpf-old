56cf7ae206f519bc04167a0381e7eec483365f44
Michael Schubert <michael@kinvolk.io>
2016-12-05 15:03:24 +0100

    Rename trace_output_kern.c -> tcptracer-bpf.c

--- ebpf.mk
+++ ebpf.mk
@@ -6,6 +6,6 @@ build:
 	@mkdir -p "$(DEST_DIR)"
 	clang -D__KERNEL__ -D__ASM_SYSREG_H \
 		-Wno-unused-value -Wno-pointer-sign -Wno-compare-distinct-pointer-types \
-		-O2 -emit-llvm -c trace_output_kern.c \
+		-O2 -emit-llvm -c tcptracer-bpf.c \
 		$(foreach path,$(LINUX_HEADERS), -I $(path)/arch/x86/include -I $(path)/arch/x86/include/generated -I $(path)/include -I $(path)/include/generated/uapi) \
 		-o - | llc -march=bpf -filetype=obj -o "${DEST_DIR}/ebpf.o"
